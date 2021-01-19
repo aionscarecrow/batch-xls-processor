3de9c3b2f25e7ad1ce3e41fc9dca098707c5b1b9
Edhar Auhushtu <augushtu@gmail.com>
2021-01-19 21:50:42 +0200

    Add logging of product summary via HashMap field in ItemWriter

---
+++ src/main/java/ua/com/foxminded/batchxlsprocessor/listener/JobCompletionNotificationListener.java
@@ -0,0 +1,33 @@
+package ua.com.foxminded.batchxlsprocessor.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ua.com.foxminded.batchxlsprocessor.writer.MapProductWriter;
+
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    @Autowired
+    private MapProductWriter mapProductWriter;
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            Set<Map.Entry<String, Double>> entries = mapProductWriter.getProductSummary().entrySet();
+            for (Map.Entry<String, Double> entry : entries) {
+                LOGGER.info("{} - {}", entry.getKey(), entry.getValue());
+            }
+        }
+    }
+}
