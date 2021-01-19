54c147cd19f82516116f5811140e8365a2e223d3
Edhar Auhushtu <augushtu@gmail.com>
2021-01-19 11:57:48 +0200

    Add template from Spring Initializr

---
+++ src/main/java/ua/com/foxminded/batchxlsprocessor/BatchXlsProcessorApplication.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.batchxlsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchXlsProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchXlsProcessorApplication.class, args);
+    }
+
+}
